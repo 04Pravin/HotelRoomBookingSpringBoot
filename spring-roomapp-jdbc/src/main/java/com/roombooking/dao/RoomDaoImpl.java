64418669ce77485c9ab51e33e72d7089e90d5670
PravinKumarKP <PravinKumarKP@Pravin-kp>
2022-10-11 07:24:59 -0700

    First commit

--- spring-roomapp-jdbc/src/main/java/com/roombooking/dao/RoomDaoImpl.java
+++ spring-roomapp-jdbc/src/main/java/com/roombooking/dao/RoomDaoImpl.java
@@ -18,6 +18,13 @@ public class RoomDaoImpl implements IRoomDao {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
+
+	/**
+	 * @param room
+	 * @return room object
+	 * functionality - addition of room
+	 */
+
 	@Override
 	public void addRoom(Room room) {
 		Object[] doctorArray = {
@@ -28,18 +35,34 @@ public class RoomDaoImpl implements IRoomDao {
 		
 	}
 
+
+	/**
+	 * @param roomNumber
+	 * @param fare
+	 * @return 
+	 * functionality - based on roomNumber the room fare will be updated and returned to the roomServiceImpl class
+	 */
 	@Override
 	public int updateRoom(int roomNumber, double fare) {
 		return jdbcTemplate.update(Queries.UPDATEQUERY,fare,roomNumber);
 		
 	}
 
+	/**
+	 * @param roomNumber
+	 * @return
+	 * functionality - based on roomNumber the room will be deleted and an int value will be return to the roomServiceImpl class
+	 */
 	@Override
 	public int deleteRoom(int roomNumber) {
 		
 		return jdbcTemplate.update(Queries.DELETEQUERY,roomNumber);
 	}
 
+	/**
+	 * @return list of rooms
+	 * functionality - list of all rooms will be return to the roomServiceImpl class
+	 */
 	@Override
 	public List<Room> findAllRooms() {
 		RowMapper<Room> mapper = new RoomMapper();
@@ -47,6 +70,10 @@ public class RoomDaoImpl implements IRoomDao {
 		return rooms;
 	}
 
+	/**
+	 * @return list of rooms
+	 * functionality - list of all rooms will be returned to the roomServiceImpl class
+	 */
 	@Override
 	public List<Room> findByLessPrice() {
 		RowMapper<Room> mapper = new RoomMapper();
@@ -54,6 +81,12 @@ public class RoomDaoImpl implements IRoomDao {
 		return rooms;
 	}
 
+	/**
+	 * @param category
+	 * @param price
+	 * @return list of rooms based on category and price
+	 * functionality - based on category and price list will be returned to the roomServiceImpl class 
+	 */
 	@Override
 	public List<Room> findByCategoryAndPrice(String category, double price) {
 		RowMapper<Room> mapper = new RoomMapper();
@@ -61,6 +94,11 @@ public class RoomDaoImpl implements IRoomDao {
 		return rooms;
 	}
 
+	/**
+	 * @param category
+	 * @return list of rooms based on category 
+	 * functionality - list of rooms based on category will be returned to the roomServiceImpl class
+	 */
 	@Override
 	public List<Room> findByCategory(String category) {
 		RowMapper<Room> mapper = new RoomMapper();
@@ -68,6 +106,12 @@ public class RoomDaoImpl implements IRoomDao {
 		return rooms;
 	}
 
+	/**
+	 * @param roomType
+	 * @param price
+	 * @return list of rooms based on type and price
+	 * functionality - list of rooms based on type and price will be returned to the roomServiceImpl class
+	 */
 	@Override
 	public List<Room> findByRoomType(String roomType, double price) {
 		RowMapper<Room> mapper = new RoomMapper();
@@ -75,6 +119,13 @@ public class RoomDaoImpl implements IRoomDao {
 		return rooms;
 	}
 
+	/**
+	 * @param startDate
+	 * @param category
+	 * @param roomType
+	 * @return list of rooms based on startDate, category and type
+	 * functionality - list of rooms based on the startDate, category and type will be returned to the roomServiceImpl class
+	 */
 	@Override
 	public List<Room> findByAvailabilityAndType(LocalDate startDate, String category, String roomType) {
 		RowMapper<Room> mapper = new RoomMapper();
@@ -82,7 +133,12 @@ public class RoomDaoImpl implements IRoomDao {
 		List<Room>rooms = jdbcTemplate.query(Queries.QUERYBYAVAILABILITYANDTYPE, mapper,roomArray);
 		return rooms;
 	}
-
+	
+	/**
+	 * @param startDate
+	 * @return list of rooms based on startDate
+	 * functionality - list of rooms based on startDate will be returned to the roomServiceImpl class
+	 */
 	@Override
 	public List<Room> findByAvailability(LocalDate startDate) {
 		RowMapper<Room> mapper = new RoomMapper();
